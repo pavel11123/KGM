87dd749c157a2362f33f788b48898cf2c47d8229
pavel11123 <korobko.11@inbox.ru>
2022-05-27 12:00:25 +0600

    add popup for callback

--- main/js/script.js
+++ main/js/script.js
@@ -7,3 +7,26 @@ function menu__close() {
    document.getElementById("phone__menu-id").style.width = "0";
    document.getElementById("phone__menu-id-body").style.overflow = "visible";
 }
+
+const popup = () => {
+   const popupBtn = document.getElementById('popup')
+   const popupModal = document.querySelector('.popup')
+
+   const cartCloseBtn = popupModal.querySelector('.cart-close')
+
+   const openPopup = () => {
+      popupModal.style.display = 'flex'
+      //popupModal.style.visibility = 'visible'
+   }
+
+   const closePopup = () => {
+      popupModal.style.display = 'none'
+      // popupModal.style.visibility = 'hidden'
+   }
+
+   popupBtn.addEventListener('click', openPopup)
+   cartCloseBtn.addEventListener('click', closePopup)
+
+}
+
+popup()
